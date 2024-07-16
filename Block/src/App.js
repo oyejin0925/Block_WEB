5663fb3e69351dc8f8767439c19d5657f791423a
Chul Hyeon <101323203+cjfgus983@users.noreply.github.com>
2024-07-16 17:04:35 +0900

    07/16 main
    프로젝트 base 커밋

---
+++ Block/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
